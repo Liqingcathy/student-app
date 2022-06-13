961756c808bb94fc6e596451d89dc5ddaa38025d
Liqingcathy <cathy.glc@gmail.com>
2022-06-12 20:51:24 -0700

    added studentlist and classinfo components

---
+++ src/components/ClassInfo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ClassInfo = () => {
+  return (
+    <section>
+      <h2>Name: React</h2>
+      <ul>
+        <li>Student Name: </li>
+        <li># of students: 30 </li>
+      </ul>
+     </section>
+  )
+
+};
+
+export default ClassInfo;
---
+++ src/components/StudentList.js
@@ -0,0 +1,7 @@
+import Reat from 'react';
+
+const StudentList = () => {
+  return <h2>Student List</h2>;
+};
+
+export default StudentList;
